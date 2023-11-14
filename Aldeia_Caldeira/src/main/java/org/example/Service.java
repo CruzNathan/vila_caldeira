07db08d165876e872a4c69f9def0b06d9fd5f078
CruzNathan <nan.nugget@hotmail.com>
2023-11-14 16:33:36 -0300

    add all

---
+++ Aldeia_Caldeira/src/main/java/org/example/Service.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Service {
+
+
+    public Task getAll(){
+
+    };
+    public Task getByID(){
+
+    };
+    public Task getByDifficulty(){
+
+    };
+    public Task getByStatus(){
+
+    };
+
+}
